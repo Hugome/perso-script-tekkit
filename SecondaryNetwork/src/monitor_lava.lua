755cbac8cff6b935b18669859b8dc0f2711f7121
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2014-09-21 04:30:05 +0200

    + Calcul of total capacity/amount/percentage

--- SecondaryNetwork/src/monitor_lava.lua
+++ SecondaryNetwork/src/monitor_lava.lua
@@ -16,3 +16,12 @@ for k,v in pairs(tank_sensor.getTargets()) do
 end
 
 print("Load tank : ", tank_1, "; ", tank_2, "; ", tank_3, "; ", tank_4, "; ")
+
+totalCapacity = tank_1.Capacity + tank_2.Capacity + tank_3.Capacity + tank_4.Capacity
+totalAmount = tank_1.Amount + tank_2.Amount + tank_3.Amount + tank_4.Amount
+percent = (totalAmount/totalCapacity)*100
+
+print("Capacity : ", totalCapacity)
+print("Amount : ", totalAmount)
+print("Ratio : ", percent, "%")
+--Command for computer : openp/github run Hugome perso-script-tekkit master SecondaryNetwork/src/monitor_lava.lua
