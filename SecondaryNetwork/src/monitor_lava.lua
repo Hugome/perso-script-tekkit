4ebb2b9fb0accb322eafc83ae6d25802f36de25d
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2014-09-21 15:59:16 +0200

    * Corrections

--- SecondaryNetwork/src/monitor_lava.lua
+++ SecondaryNetwork/src/monitor_lava.lua
@@ -5,52 +5,31 @@ os.loadAPI("ocs/apis/sensor")
 m = peripheral.wrap("monitor_0")
 tank_sensor = sensor.wrap("top")
 
-
---Init
-tanks = {}
-nb_tanks = 0
-
-
-for pos, target in pairs(tank_sensor.getTargets()) do 
-  if(pos == "0,0,3" or pos == "0,0,4" or pos == "1,0,3" or pos == "1,0,4") then
-    local id = "tank_" .. nb_tanks
-    tanks[id] = tank_sensor.getTargetDetails(pos)
-    nb_tanks = nb_tanks + 1
-  end
-end
-
-print("Load tank : ")
-for id_tank = 0, (nb_tanks - 1) do
-  local tank = tanks["tank_" .. id_tank]
-  print(textutils.serialize(tank.Name) .. " id => tank_" .. id_tank)
-end
-
 while true do
   --Update
   local totalCapacity = 0
   local totalAmount = 0
-  for id_tank = 0, (nb_tanks - 1) do
-    local tank = tanks["tank_" .. id_tank]
-    totalCapacity = totalCapacity + tank.Tanks[1].Capacity
-    totalAmount = totalAmount + tank.Tanks[1].Amount
+  for pos, target in pairs(tank_sensor.getTargets()) do 
+    if(pos == "0,0,3" or pos == "0,0,4" or pos == "1,0,3" or pos == "1,0,4") then
+      local tank = tank_sensor.getTargetDetails(pos)
+      totalCapacity = totalCapacity + tank.Tanks[1].Capacity
+      totalAmount = totalAmount + tank.Tanks[1].Amount
+    end
   end
   local percent = (totalAmount/totalCapacity)*100
   
   --Graphics update
   m.clear()
   m.setCursorPos(1, 1)
-  m.write("Réseau secondaire, stockage de lave")
+  m.write("  Stockage de lave  ")
   m.setCursorPos(1, 2)
-  m.write("Capacité : " .. totalCapacity .. " mB")
+  m.write("Capacite : " .. totalCapacity .. " mB")
   m.setCursorPos(1, 3)
   m.write("Rempli : " .. totalAmount .. " mB")
   m.setCursorPos(1, 4)
   m.write("Plein a " .. percent .. " %")
+  sleep(0.5)
 end
 
-
---print("Capacity : ", totalCapacity)
---print("Amount : ", totalAmount)
---print("Ratio : ", percent, "%")
 --Command for computer : openp/github run Hugome perso-script-tekkit master SecondaryNetwork/src/monitor_lava.lua
 --Command for computer : openp/github get Hugome perso-script-tekkit master SecondaryNetwork/src/monitor_lava.lua startup
