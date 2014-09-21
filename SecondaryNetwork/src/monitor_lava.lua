a54a20e13f2d93ecd3e9bb9661cc43f3fcc9c6a1
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2014-09-21 04:33:06 +0200

    * Name of variable * Try to resolve tank_* var

--- SecondaryNetwork/src/monitor_lava.lua
+++ SecondaryNetwork/src/monitor_lava.lua
@@ -7,11 +7,11 @@ tank_sensor = sensor.wrap("top")
 
 
 --Init
-for k,v in pairs(tank_sensor.getTargets()) do 
-  if(k == "0,0,3") then tank_1 = v;
-  elseif(k == "0,0,4") then tank_2 = v;
-  elseif(k == "1,0,3") then tank_3 = v;
-  elseif(k == "1,0,4") then tank_4 = v;
+for name, target in pairs(tank_sensor.getTargets()) do 
+  if(k == "0,0,3")      then tank_1 = tank_sensor.getTargetDetails(name);
+  elseif(k == "0,0,4")  then tank_2 = tank_sensor.getTargetDetails(name);
+  elseif(k == "1,0,3")  then tank_3 = tank_sensor.getTargetDetails(name);
+  elseif(k == "1,0,4")  then tank_4 = tank_sensor.getTargetDetails(name);
   end
 end
 
