e391adb6d30bd4f68cfa47bdab27a374eba6a2a2
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2014-09-21 23:15:04 +0200

    + Return Home Functionality * Fix refuel function 	Select to 1 	Refuel one time

--- TurtleAdvancedMining/src/simple_turtle.lua
+++ TurtleAdvancedMining/src/simple_turtle.lua
@@ -1,9 +1,40 @@
 --Peripheral var
-print("Longeur du tunnel (forcez un peu elle revient toute seul : ")
+print("Longeur du tunnel (forcez un peu elle revient toute seul): ")
 longeur_tunnel = tonumber(read())
 digged_tunnel = 0
 
 --Functions
+function ifReturnHome()
+  local emptySlot = 0;
+  for i = 1, 16 do
+    if(turtle.getItemCount(i) == 0) then emptySlot = emptySlot + 1 end
+  end
+  if(emptySlot >= 1)then return false
+  else return true
+  end
+end
+function checkReturnHome()
+  if(ifReturnHome) then
+    turtle.turnRight()
+    turtle.turnRight()
+    for i = 1, digged_tunnel do
+      checkFuel()
+      turtle.forward()
+    end
+    for i = 1, 16 do
+      turtle.select(i)
+      if (turtle.refuel(0) == false) then turtle.dropUp() end
+    end
+    turtle.turnRight()
+    turtle.turnRight()
+    for i = 1, digged_tunnel do
+      checkFuel()
+      turtle.forward()
+    end
+  end
+end
+
+
 function getRestToDig()
   return (longeur_tunnel - digged_tunnel)
 end
@@ -15,8 +46,11 @@ function checkFuel()
     turtle.select(i)
     if turtle.refuel(0) then
       turtle.refuel(10)
+      turtle.select(1)
+      return
     end
   end
+  turtle.select(1)
 end
 
 function whileDig()
@@ -53,8 +87,10 @@ function digOneTranche()
   digged_tunnel = digged_tunnel + 1
 end
 
+--Main while
 while (longeur_tunnel > digged_tunnel) do
   digOneTranche()
+  checkReturnHome()
   sleep(0.5)
 end
 
