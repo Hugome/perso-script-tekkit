21791d83f3be11e6ab07f1d15912cdc07e0535aa
Hugo Meyronneinc <hugomeyronneinc@gmail.com>
2014-09-21 15:41:40 +0200

    + Rendering functionality

--- SecondaryNetwork/src/monitor_lava.lua
+++ SecondaryNetwork/src/monitor_lava.lua
@@ -20,14 +20,34 @@ for pos, target in pairs(tank_sensor.getTargets()) do
 end
 
 print("Load tank : ")
-for id_tank = 0, nb_tanks do
+for id_tank = 0, (nb_tanks - 1) do
   local tank = tanks["tank_" .. id_tank]
-  print(textutils.serialize(tank.Name))
+  print(textutils.serialize(tank.Name) .. " id => tank_" .. id_tank)
 end
 
---totalCapacity = tank_1.Capacity + tank_2.Capacity + tank_3.Capacity + tank_4.Capacity
---totalAmount = tank_1.Amount + tank_2.Amount + tank_3.Amount + tank_4.Amount
---percent = (totalAmount/totalCapacity)*100
+while true do
+  --Update
+  local totalCapacity = 0
+  local totalAmount = 0
+  for id_tank = 0, (nb_tanks - 1) do
+    local tank = tanks["tank_" .. id_tank]
+    totalCapacity = totalCapacity + tank.Tanks[1].Capacity
+    totalAmount = totalAmount + tank.Tanks[1].Amount
+  end
+  local percent = (totalAmount/totalCapacity)*100
+  
+  --Graphics update
+  m.clear()
+  m.setCursorPos(1, 1)
+  m.write("Réseau secondaire, stockage de lave")
+  m.setCursorPos(1, 2)
+  m.write("Capacité : " .. totalCapacity .. " mB")
+  m.setCursorPos(1, 3)
+  m.write("Rempli : " .. totalAmount .. " mB")
+  m.setCursorPos(1, 4)
+  m.write("Plein a " .. percent .. " %")
+end
+
 
 --print("Capacity : ", totalCapacity)
 --print("Amount : ", totalAmount)
