75f9367852ac91e18be9e8dcbd32ede3b8025166
Hugo Meyronneinc <hugo@hugome.eu>
2015-06-17 01:03:20 +0200

    ObsidianGenerator

--- ObsidianGenerator/src/obsidian_gen.lua
+++ ObsidianGenerator/src/obsidian_gen.lua
@@ -1,7 +1,7 @@
 toBreak = 0
 breaked = 0
 
-redstone.setOutput("right", false)
+redstone.setOutput("left", false)
 -- Count obsidian to generate
 for i = 1, 16 do
   local slotDetails = turtle.getItemDetail()
@@ -27,11 +27,11 @@ end
 function slotToObsi()
   local slotDetails = turtle.getItemDetail()
   while (slotDetails ~= nil and slotDetails.name == "minecraft:redstone" and turtle.getItemCount() > 0) do
-  redstone.setOutput("right", false)
+  redstone.setOutput("left", false)
   turtle.place()
-  redstone.setOutput("right", true)
+  redstone.setOutput("left", true)
   sleep(0.5)
-  redstone.setOutput("right", false)
+  redstone.setOutput("left", false)
   turtle.dig()
   breaked = breaked + 1
   updateDisplay()
@@ -44,7 +44,7 @@ for i = 1, 16 do
   turtle.select(i)
   slotToObsi()
 end
-redstone.setOutput("right", false)
+redstone.setOutput("left", false)
 turtle.select(1)
 
 --Command for computer : openp/github run Hugome perso-script-tekkit master ObsidianGenerator/src/obsidian_gen.lua
