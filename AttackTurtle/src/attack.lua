20497971b5c72dee6dae8e5fb7b70437d949f8a5
Hugo Meyronneinc <hugo@hugome.eu>
2015-06-20 03:17:25 +0200

    Update ObsidianGen and attack

---
+++ AttackTurtle/src/attack.lua
@@ -0,0 +1,8 @@
+while true then
+      os.queueEvent("randomEvent")
+      os.pullEvent()
+      turtle.attack()
+end
+
+--Command for computer : openp/github run Hugome perso-script-tekkit master AttackTurtle/src/attack.lua
+--Command for computer : openp/github get Hugome perso-script-tekkit master AttackTurtle/src/attack.lua startup
